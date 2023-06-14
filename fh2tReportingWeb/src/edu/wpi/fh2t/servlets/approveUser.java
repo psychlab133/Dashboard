c8d74253da87da872d019208d5a156afe2b56290
tysomasse <tysomasse@wpi.edu>
2023-06-14 14:29:38 -0400

    test commit

--- fh2tReportingWeb/src/edu/wpi/fh2t/servlets/approveUser.java
+++ fh2tReportingWeb/src/edu/wpi/fh2t/servlets/approveUser.java
@@ -1,16 +1,6 @@
 package edu.wpi.fh2t.servlets;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+lol
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
